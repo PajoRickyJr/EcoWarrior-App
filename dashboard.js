814b6f322dfd3f3323602a38be478af65a8881e0
PajoRickyJr <rickypajojr2003@gmail.com>
2025-03-17 22:25:55 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,11 @@
 document.getElementById('userBox').addEventListener('click', function () {
     // For prototyping: store the selected role and navigate
     localStorage.setItem('role', 'user');
-    window.location.href = 'UserSelect/userNew.html';
+    window.location.href = 'userNew.html';
   });
   
   document.getElementById('adminBox').addEventListener('click', function () {
     localStorage.setItem('role', 'admin');
-    window.location.href = 'AdminSelect/adminLogin.html';
+    window.location.href = 'adminLogin.html';
   });
   
